ee8fa9f396fe5acc6398c74d6ee3b7a7991d65fa
wyclif <wyclif@openmrs.org>
2018-07-18 11:45:33 -0400

    Switched to using env to set mongodb host and port in dockerize to wait for the mongodb instance

--- boot.sh
+++ boot.sh
@@ -3,7 +3,7 @@
 set -e
 
 #Start openhim-core using dockerize so that we can wait for mongodb to be ready
-dockerize -wait tcp://openhim-mongo:27017 -timeout 15s ./start-openhim.sh run &
+dockerize -wait tcp://${mongo_host}:${mongo_port} -timeout 90s ./start-openhim.sh run &
 
 #Start the mediator using dockerize so that we can wait for openhim core to be ready
-dockerize -wait tcp://localhost:5002 -timeout 15s mediator/start-mediator.sh run
+dockerize -wait tcp://localhost:5002 -timeout 90s mediator/start-mediator.sh run
